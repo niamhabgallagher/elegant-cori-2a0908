5ad2540473c4ce1602286af2f976a6d18ca95d99
Niamh Gallagher <ngallagher@vtime.local>
2021-11-04 15:55:25 +0000

    maybe a fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
-<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 <template>
   <div>Hi</div>
 </template>
 
 <script>
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
