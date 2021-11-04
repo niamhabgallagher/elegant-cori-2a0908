213f290eb2ae736fbc65951d25c4b1a2814fecd1
Niamh Gallagher <ngallagher@vtime.local>
2021-11-04 15:22:00 +0000

    more

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
+<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 <template>
   <div>Hi</div>
 </template>
+
+<script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+</script>
