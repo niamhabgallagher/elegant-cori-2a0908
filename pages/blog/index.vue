42555cadcc8b35baadf22dba3a8b09697640e211
Niamh Gallagher <ngallagher@vtime.local>
2021-11-05 11:53:38 +0000

    renaming folder

---
+++ pages/blog/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Blog Posts</h1>
+    <v-list v-for="post of posts" :key="post.slug">
+      <v-list-item>
+        <nuxt-link :to="post.slug">{{ post.title }}</nuxt-link>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({$content}) {
+    const posts = await $content("blog").fetch();
+    // eslint-disable-next-line no-console
+    console.log(posts);
+
+    return {
+      posts,
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Hi</div>
+  <div>
+    <h1>Home Page</h1>
+  </div>
 </template>
 
 <script>
